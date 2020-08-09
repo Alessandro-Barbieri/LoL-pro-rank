0f6683c2de4171ea6c4c73131c04b2d2b7e7e717
Alessandro-Barbieri <lssndrbarbieri@gmail.com>
2020-08-09 09:21:24 +0200

    new stuff

--- TS.py
+++ TS.py
@@ -1,36 +1,97 @@
-import csv, json, glob
-from trueskill import Rating, rate, setup
-from trueskill.backends import available_backends
+import csv, json
 from datetime import datetime
-from collections import defaultdict, OrderedDict
+from collections import defaultdict
 from math import floor
-from operator import itemgetter
-from itertools import chain
+from trueskill import Rating, rate, setup, expose
+from trueskill.backends import available_backends
+
+EXTRA = True
 
 if 'mpmath' in available_backends():
     setup(backend='mpmath')
 
-storico = defaultdict(dict)
-partite = OrderedDict()
-with open('partite.tsv', 'rt', encoding = 'utf-8-sig', ) as f:
+giocatori = {}
+with open('giocatori.tsv', 'rt', encoding='utf-8-sig') as f:
+	reader = csv.reader(f, delimiter="\t")
+	for riga in reader:
+		giocatore = str.upper(riga[1])
+		alias = str.upper(riga[0])
+		giocatori[alias] = giocatore
+		giocatori[giocatore] = giocatore
+
+storico = {}
+partite = {}
+with open('partite.tsv', 'rt', encoding='utf-8-sig') as f:
 	reader = csv.reader(f, delimiter="\t")
 	for riga in reader:
 		data = datetime.strptime(riga[0], '%Y-%m-%d %H:%M:%S')
-		id = riga[1]
+		ide = riga[1]
 		nome = riga[3]
-		risultato = riga[2]
-		if id not in partite.keys():
-			partite[id] = {'data': data, 'Yes': list(), 'No': list()}
+		risultato = riga[4]
+		squadra1 = riga[5].split(',')
+		squadra2 = riga[6].split(',')
+		if risultato == "2":
+			vincitori = squadra2
+			perdenti = squadra1
+		elif risultato == "1":
+			vincitori = squadra1
+			perdenti = squadra2
+		else:
+			print('ERRORE')
 
-		#try:
-		partite[id][risultato].append(nome)
-		#except:
-		#	continue
+		partite[ide] = {'data': data, 'Yes': vincitori, 'No': perdenti}
+
+n = 0
+if EXTRA:
+	with open('extra.tsv', 'rt', encoding='utf-8-sig') as f:
+		reader = csv.reader(f, delimiter="\t")
+		for riga in reader:
+			n = n + 1
+			data = datetime.strptime(riga[1], '%d/%m/%y')
+			ide = "".join([riga[2], data.date().isoformat(), str(n)])
+			squadra1 = riga[7:12]
+			squadra2 = riga[12:17]
+			r1 = riga[5]
+			r2 = riga[6]
+			if (('0' == r1) and ('1' == r2)):
+				vincitori = squadra2
+				perdenti = squadra1
+			elif (('1' == r1) and ('0' == r2)):
+				vincitori = squadra1
+				perdenti = squadra2
+			else:
+				print('ERRORE')
+
+			partite[ide] = {'data': data, 'Yes': vincitori, 'No': perdenti}
+
+for c,v in partite.items():
+	vincitori = []
+	perdenti = []
+	for alias in v['Yes']:
+		ALIAS = str.upper(alias)
+		if ALIAS not in giocatori.keys():
+			giocatori[ALIAS] = ALIAS
+
+		nome = giocatori[ALIAS]
+		vincitori.append(nome)
 		if nome not in storico:
 			storico[nome] = {'date': defaultdict(list)}
 
+	v['Yes'] = vincitori
+	for alias in v['No']:
+		ALIAS = str.upper(alias)
+		if ALIAS not in giocatori.keys():
+			giocatori[ALIAS] = ALIAS
+
+		nome = giocatori[ALIAS]
+		perdenti.append(nome)
+		if nome not in storico:
+			storico[nome] = {'date': defaultdict(list)}
+			
+	v['No'] = perdenti
+
 punteggi = {}
-for k,v in partite.items():
+for v in sorted(partite.values(), key=lambda x: x['data']):
 	squadra1 = dict()
 	squadra2 = dict()
 	vincitori = v['Yes']
@@ -49,29 +110,28 @@ for k,v in partite.items():
 		squadra2[g] = punteggi[g]
 
 	t = v['data'].date().isoformat()
-#	print(t, k)
-	ris1, ris2 = rate([squadra1, squadra2], ranks = [1,2])
+	ris1, ris2 = rate([squadra1, squadra2], ranks=[1, 2])
 	for p1 in ris1.keys():
 		s = ris1[p1].sigma
 		m = ris1[p1].mu
+		r = expose(ris1[p1])
 		punteggi[p1] = ris1[p1]
-		storico[p1]['date'][t] = (m, s)
+		storico[p1]['date'][t] = {'mi': m, 'sigma': s, 'r': r}
 
 	for p2 in ris2.keys():
 		s = ris2[p2].sigma
 		m = ris2[p2].mu
+		r = expose(ris2[p2])
 		punteggi[p2] = ris2[p2]
-		storico[p2]['date'][t] = (m, s)
+		storico[p2]['date'][t] = {'mi': m, 'sigma': s, 'r': r}
 
 with open('storico.json', 'w', encoding='utf8') as file:
-	json.dump(storico, file, sort_keys = True, indent = "\t", ensure_ascii = False)
+	json.dump(storico, file, sort_keys=True, indent="\t", ensure_ascii=False)
 
 g = []
 for nome, val in storico.items():
 	l = sorted(list(val['date'].items()))
-	mi = l[-1][1][0]
-	sigma = l[-1][1][1]
-	r = mi - 2*sigma
+	r = l[-1][1]['r']
 	g.append((r, nome))
 
 ordinati = sorted(g, reverse=True)
--- scarica.py
+++ scarica.py
@@ -3,17 +3,15 @@ import csv
 
 site = mwclient.Site('lol.gamepedia.com', path="/")
 limite = 500
-i = 588
-with open('partite2.tsv', 'w', newline='', encoding='utf8') as file:
+i=0
+with open('giocatori.tsv', 'w', newline='', encoding='utf8') as file:
 	writer = csv.writer(file, delimiter="\t", quoting=csv.QUOTE_ALL)
 	while True:
 		print(i)
 		response=site.api('cargoquery',
 					limit = limite,
-					tables = "ScoreboardGames=SG, ScoreboardPlayers=SP, PlayerRedirects=PR",
-					fields = "SG.UniqueGame, SG.DateTime_UTC, SP.PlayerWin, COALESCE(PR.OverviewPage,SP.Link)=N",
-					join_on = "SG.UniqueGame=SP.UniqueGame, SP.Link=PR.AllName",
-					order_by = "SG.DateTime_UTC, SP.UniqueGame",
+					tables = "PlayerRedirects=PR",
+					fields = "PR.AllName, PR.OverviewPage",
 					offset = i * limite
 		)
 		if not response['cargoquery']:
@@ -21,6 +19,27 @@ with open('partite2.tsv', 'w', newline='', encoding='utf8') as file:
 
 		for partita in response['cargoquery']:
 			p = partita['title']
-			writer.writerow([p['DateTime UTC'], p['UniqueGame'], p['PlayerWin'], p['N']])
+			writer.writerow([p['AllName'], p['OverviewPage']])
+
+		i = i + 1
+
+i = 0
+with open('partite.tsv', 'w', newline='', encoding='utf8') as file:
+	writer = csv.writer(file, delimiter="\t", quoting=csv.QUOTE_ALL)
+	while True:
+		print(i)
+		response=site.api('cargoquery',
+					limit = limite,
+					tables = "ScoreboardGames=SG",
+					fields = "SG.UniqueGame, SG.DateTime_UTC, SG.Team1, SG.Team2, SG.Winner, SG.Team1Links, SG.Team2Links",
+					order_by = "SG.DateTime_UTC",
+					offset = i * limite
+		)
+		if not response['cargoquery']:
+			break
+
+		for partita in response['cargoquery']:
+			p = partita['title']
+			writer.writerow([p['DateTime UTC'], p['UniqueGame'], p['Team1'], p['Team2'], p['Winner'], p['Team1Links'], p['Team2Links']])
 
 		i = i + 1
